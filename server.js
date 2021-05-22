3cc1c217ec804b76e850e8e91aa3070619c0b7ba
ARCHANA N <79159935+archana2330@users.noreply.github.com>
2021-05-22 18:38:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express'); 
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.get('/',function(req,resp)
+{
+	resp.sendFile(__dirname+"/"+"public/index.html")
+});
+app.get('/stock',function(req,resp)
+{
+	resp.sendFile(__dirname+"/"+"public/stock.html")
+});
+app.post('/login',function(req,resp)
+{
+	if(req.body.username=='admin'&& req.body.password=='admin123')
+	{
+		console.log('Logged in successfully');
+		resp.sendFile(__dirname+"/"+"public/success.html")
+	}
+	else
+	{
+		console.log('invalid credentials');
+		resp.sendFile(__dirname+"/"+"public/error.html")
+	}
+});
+app.listen(7775,function(){
+	console.log('Listening at port 7775');
+});
